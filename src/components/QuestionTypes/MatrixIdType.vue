a0b5b5ec3cc574050d24c011fd7020e5a4d717ae
Bernhard Bliem <bernhard@bliem.xyz>
2021-09-20 11:01:56 +0200

    Make Matrix ID regex conform a bit more to the specification

--- src/components/QuestionTypes/MatrixIdType.vue
+++ src/components/QuestionTypes/MatrixIdType.vue
@@ -14,7 +14,10 @@
     methods: {
       validate() {
         if (this.hasValue) {
-          return /^@[^@:.]+:[^@:.]+\.[^@:.]+$/.test(this.dataValue)
+          // https://matrix.org/docs/spec/appendices#identifier-grammar
+          // Actually upper-case characters are not allowed in the local part, but we might want to accept and downcase
+          // them. Or maybe the server does it automatically?
+          return /^@[0-9a-zA-Z.=_/-]+:[0-9a-zA-Z:.-]+$/.test(this.dataValue)
         }
 
         return !this.question.required
